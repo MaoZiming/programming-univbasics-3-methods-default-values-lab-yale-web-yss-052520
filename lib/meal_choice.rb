cdc0ceeb9ba43c1a849f36f909ad3e081e8fded7
Ziming Mao <ziming.mao@yale.edu>
2020-05-01 18:34:09 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,7 @@
 
 def meal_choice (veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
-  return "A plate of lentils with carrots and string beans." if veg1 == "carrots" && veg2 == "string beans" && protein == "lentils"
-  return " plate of tofu with carrots and string beans." if veg1 == "carrots" && veg2 == "string beans" && protein == "tofu"
+  meal =  "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts meal
+  return meal
+end
